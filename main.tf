1e547ed0cc2df6543d8ea9cf9db2066c3475b24f
Sivakumar S <sivakumarit42@gmail.com>
2023-03-23 18:45:45 +0530

    changes

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ resource "aws_launch_template" "main" {
 
 
 
-  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
-    component = var.component
-    env       = var.env
-  }))
+#  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+#    component = var.component
+#    env       = var.env
+#  }))
 }
 
 resource "aws_autoscaling_group" "main" {
