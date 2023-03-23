6661c5419b2c2d92783159060df89588b306c7e0
Sivakumar S <sivakumarit42@gmail.com>
2023-03-23 18:25:09 +0530

    changes

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+
+  //  iam_instance_profile {
+  //    name = "test"
+  //  }
+
+  image_id = data.aws_ami.ami.id
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  instance_type = var.instance_type
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      var.tags,
+      { Name = "${var.component}-${var.env}" }
+    )
+  }
+
+
+
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    component = var.component
+    env       = var.env
+  }))
+}
+
+resource "aws_autoscaling_group" "main" {
+  name                = "${var.component}-${var.env}"
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = false
+    value               = "${var.component}-${var.env}"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "tags" {}
+variable "component" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "subnets" {}
