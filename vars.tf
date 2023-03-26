af24fcda8ad10eb0d7de088f41f971f84ee93a59
Sivakumar S <sivakumarit42@gmail.com>
2023-03-26 10:32:47 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -6,3 +6,11 @@ variable "desired_capacity" {}
 variable "min_size" {}
 variable "max_size" {}
 variable "subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "allow_app_to" {}
+variable "port" {}
+variable "dns_domain" {}
+variable "alb_dbs_name" {}
+variable "listener_arn" {}
+variable "listener_priority" {}
