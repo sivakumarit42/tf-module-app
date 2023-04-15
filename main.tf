54f57eb3c29418322bc38413a3cf1a04fba6aa9c
DELL <sivakumarit42@gmail.com>
2023-04-15 09:12:57 +0530

    changes

--- main.tf
+++ main.tf
@@ -55,7 +55,9 @@ resource "aws_autoscaling_policy" "asg-cpu-rule" {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-    target_value = 40.0
+    # target_value = 40.0
+    target_value = 20.0
+
   }
 }
 
